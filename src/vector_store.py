7deb6ab8291bc2f5408222f23c6db75173f64ca9
247201-sam <247201@students.au.edu.pk>
2026-07-21 21:38:23 +0500

    Update vector_store.py

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,1 +1,32 @@
+import streamlit as st
+from pinecone import Pinecone, ServerlessSpec
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from config import EMBEDDING_MODEL_NAME
 
+@st.cache_resource
+def load_embedding_model():
+    return HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+
+def index_to_pinecone(chunks, pinecone_key, index_name, namespace):
+    pc = Pinecone(api_key=pinecone_key)
+    
+    existing_indexes = [idx.name for idx in pc.list_indexes()]
+    if index_name not in existing_indexes:
+        pc.create_index(
+            name=index_name,
+            dimension=384,
+            metric="cosine",
+            spec=ServerlessSpec(cloud="aws", region="us-east-1")
+        )
+
+    embeddings = load_embedding_model()
+    
+    vector_store = PineconeVectorStore.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        index_name=index_name,
+        namespace=namespace,
+        pinecone_api_key=pinecone_key
+    )
+    return vector_store
